fbe0d3f0d94aea4e3a74c3ef2c9fdc1d032d3bfb
vertesy <a.vertesy@hubrecht.eu>
2016-03-18 19:11:57 +0100

    Markdown table parsers in python

---
+++ Python/MarkdownParsers/md.description_parser.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+'''
+### md.description_parser.py
+	- Parse a github wiki page from the script headers
+'''
+import os, glob, re
+
+# setup ------------------------------------------------
+Outfile ="/Users/abelvertesy/x_reactivation.wiki/Analysis_Methods/Pipeline-Diff-Mapping.md"
+ScriptDir = "/Users/abelvertesy/x_reactivation/analysis/DevCell_analysis/"
+GitRepo = "https://github.com/bow/x_reactivation/"
+
+
+# Prepare ------------------------------------------------
+GitCode = GitRepo+"blob/master/analysis/DevCell_analysis/"
+GitWikiPage = GitRepo+"wiki/"+re.split( '[/\.]', Outfile)[-2]
+
+os.chdir(ScriptDir)
+ls_Scripts = glob.glob("*.py")
+ls_Scripts.extend(glob.glob("*.sh"))
+ls_Scripts.extend(glob.glob("*.R"))
+
+ls_Scripts= sorted(ls_Scripts); print ls_Scripts
+
+Header=["# Differential Mapping Pipeline",
+	"## Table of Contents"
+	]
+
+Markdown_file=open(Outfile,'w')					# Create Header
+for l in Header:
+	Markdown_file.writelines(l)
+	Markdown_file.write("\n\n")
+
+# Create Table of Contents ------------------------------------------------------
+
+Markdown_file=open(Outfile,'a')
+in_header = False
+for s in ls_Scripts:
+	with open(s) as Script:
+		for line in Script:
+			if line.startswith('\'\'\''):
+				in_header = not in_header
+			if in_header:
+				if line.startswith('###'):
+					script_fname = (line.split('### ')[1]).rstrip()
+					entry= '- ['+script_fname+"\n"+']('+GitWikiPage+'#-'+(script_fname.replace('.','')).lower()+")\n"
+					print entry
+					Markdown_file.write(entry)
+Markdown_file.write( "\n## Detailed Script Explanation \n")
+
+# Creat Body ------------------------------------------------------
+Markdown_file=open(Outfile,'a')
+in_header = False
+for s in ls_Scripts:
+	with open(s) as Script:
+		for line in Script:
+			if line.startswith('\'\'\''):
+				in_header = not in_header
+			if in_header:
+				if line.startswith('\'\'\''):
+					Markdown_file.write("\n\n")
+				else:
+					print line
+					if line.startswith('###'):
+						entry = line.split('### ')[1].rstrip()
+						line = '### [ '+entry+']('+GitCode+entry+")\n" 				# header with link
+					Markdown_file.write(line)
+
+Markdown_file.close()
+print 'DONE'
+
---
+++ Python/MarkdownParsers/md.tsv_to_md_parser.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+'''
+### md.tsv_to_md_parser.py
+- read a tab delimeted file and print the output in [github] .md format
+- Usage: script tsv_filepath
+'''
+import csv, sys
+
+write_file = True
+
+tsv=sys.argv[1]
+Outfile= tsv+'.md'
+print Outfile
+
+# Execute --------------------------------------------------------
+Markdown_file=open(Outfile,'w'); Markdown_file.close()				# Overwrite
+Markdown_file=open(Outfile,'a')
+
+i=0
+with open(tsv, 'rU') as textfile:									# U for Universal lien ending
+	for row in (list(csv.reader(textfile, delimiter='\t'))):
+		i +=1
+		if i ==1:
+			nr_cols = len(row)
+
+			line= '| '+" \t| ".join(row)+' |'
+			if write_file: 	Markdown_file.write(line+"\n")
+			print line
+
+			line = ('|---'*nr_cols)+'|'
+			if write_file: 	Markdown_file.write(line+"\n")
+			print line
+
+		else:
+			line= '| '+" \t| ".join(row)+' |'
+			if write_file: 	Markdown_file.write(line+"\n")
+			print line
+
+
+print "\n\nDONE"
