1beffcee4370b706c04214b3bb3b342a0cd9f2b7
vertesy <a.vertesy@hubrecht.eu>
2016-05-10 12:01:14 +0200

    ensebl link parser for mice

--- R/GetDatabaseLinks.R
+++ R/GetDatabaseLinks.R
@@ -10,6 +10,8 @@ HGNC_symbol_search = "http://www.genenames.org/cgi-bin/gene_search?search="
 grc37 = c("http://grch37.ensembl.org/Human/Search/Results?q=", ";site=ensembl;facet_feature_type=Gene;facet_species=Human")
 grc38 = c("http://www.ensembl.org/Human/Search/Results?q=", ";site=ensembl;facet_feature_type=Gene;facet_species=Human")
 
+grc_mm38 = c("http://www.ensembl.org/Mouse/Search/Results?q=", ";site=ensembl;facet_feature_type=;facet_species=Mouse")
+
 
 
 # Functions ------------------------
@@ -23,13 +25,22 @@ link_HGNC <- function (vector_of_gene_symbols, writeOut = T) { # Parse HGNC link
 }
 
 
+link_ensembl_mice <- function (vector_of_gene_symbols, writeOut = T) { # Parse the latest ensembl (GRC38) links to your list of gene symbols
+  links = paste0(grc_mm38[1], vector_of_gene_symbols, grc_mm38[2])
+  if (writeOut) {
+    bash_commands = paste0("open ", links)
+    write.simple.append("", ManualName = "/Users/abelvertesy/bin/run.sh")
+    write.simple.append(bash_commands, ManualName = "/Users/abelvertesy/bin/run.sh")
+  } else { return(links) }
+}
+
 link_ensembl <- function (vector_of_gene_symbols, writeOut = T) { # Parse the latest ensembl (GRC38) links to your list of gene symbols
-	links = paste0(grc38[1], vector_of_gene_symbols, grc38[2])
-	if (writeOut) {
-		bash_commands = paste0("open ", links)
-		write.simple.append("", ManualName = "/Users/abelvertesy/bin/run.sh")
-		write.simple.append(bash_commands, ManualName = "/Users/abelvertesy/bin/run.sh")
-	} else { return(links) }
+  links = paste0(grc38[1], vector_of_gene_symbols, grc38[2])
+  if (writeOut) {
+    bash_commands = paste0("open ", links)
+    write.simple.append("", ManualName = "/Users/abelvertesy/bin/run.sh")
+    write.simple.append(bash_commands, ManualName = "/Users/abelvertesy/bin/run.sh")
+  } else { return(links) }
 }
 
 
