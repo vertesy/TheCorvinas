c1c52fc5ef8a2d8faaa8b3961ae72e8643a85919
vertesy <a.vertesy@hubrecht.eu>
2016-06-17 17:21:15 +0200

    link_ensembl_zebra()

--- R/GetDatabaseLinks.R
+++ R/GetDatabaseLinks.R
@@ -12,6 +12,10 @@ grc38 = c("http://www.ensembl.org/Human/Search/Results?q=", ";site=ensembl;facet
 
 grc_mm38 = c("http://www.ensembl.org/Mouse/Search/Results?q=", ";site=ensembl;facet_feature_type=;facet_species=Mouse")
 
+
+grc_Zebra = c("http://www.ensembl.org/Mouse/Search/Results?q=", ";site=ensembl;facet_feature_type=;facet_species=Zebrafish")
+
+
 uniprot_mouse = c('http://www.uniprot.org/uniprot/?query=organism%3A"Mus+musculus+[10090]"+',"&sort=score")
 # I left out (Mouse) aprt because it messes up with markdown, it still works.
 uniprot_human = c('http://www.uniprot.org/uniprot/?query=organism%3A"Homo+sapiens+%28Human%29+[9606]"+',"&sort=score")
@@ -28,6 +32,24 @@ link_HGNC <- function (vector_of_gene_symbols, writeOut = T) { # Parse HGNC link
 }
 
 # ENSEMBL Links --------------------------------------------------------------------------------------------------------------------------------
+
+link_ensembl_zebra <- function (vector_of_gene_symbols, writeOut = T) { # Parse the latest ensembl (GRC38) links to your list of gene symbols
+  links = paste0(grc_mm38[1], vector_of_gene_symbols, grc_Zebra[2])
+  if (writeOut) {
+    bash_commands = paste0("open ", links)
+    write.simple.append("", ManualName = "/Users/abelvertesy/bin/run.sh")
+    write.simple.append(bash_commands, ManualName = "/Users/abelvertesy/bin/run.sh")
+  } else { return(links) }
+}
+
+link_ensembl_mice <- function (vector_of_gene_symbols, writeOut = T) { # Parse the latest ensembl (GRC38) links to your list of gene symbols
+  links = paste0(grc_mm38[1], vector_of_gene_symbols, grc_mm38[2])
+  if (writeOut) {
+    bash_commands = paste0("open ", links)
+    write.simple.append("", ManualName = "/Users/abelvertesy/bin/run.sh")
+    write.simple.append(bash_commands, ManualName = "/Users/abelvertesy/bin/run.sh")
+  } else { return(links) }
+}
 
 link_ensembl_mice <- function (vector_of_gene_symbols, writeOut = T) { # Parse the latest ensembl (GRC38) links to your list of gene symbols
   links = paste0(grc_mm38[1], vector_of_gene_symbols, grc_mm38[2])
