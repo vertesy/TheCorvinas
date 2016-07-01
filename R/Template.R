05f3bd39e9b5006579a5aef5509ba5cd7df510a0
vertesy <a.vertesy@hubrecht.eu>
2016-07-01 21:09:12 +0200

    template script

---
+++ R/Template.R
@@ -0,0 +1,25 @@
+######################################################################
+# Description
+######################################################################
+# source ('')
+# rm(list=ls(all.names = TRUE)); try(dev.off(), silent = T)
+
+# Functions ------------------------
+try (source ('/Users/abelvertesy/Github_repos/TheCorvinas/R/CodeAndRoll.R'),silent= F)
+try (source("/Users/abelvertesy/Github_repos/Spermatogenesis/Mapping/functions_sp.r") , silent= F)
+source("/Users/abelvertesy/Github_repos/hmC/RaceID/RaceID2_class_ext.2016.05.10.R") ## load class definition and functions
+source ('/Users/abelvertesy/Github_repos/TheCorvinas/R/DatabaseLinke.r')
+
+# Setup ------------------------
+
+# Metadata ------------------------
+
+# Parameters ------------------------
+
+# Read In ------------------------
+
+# QC ------------------------
+
+# Plot ------------------------
+
+
