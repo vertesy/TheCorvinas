bbc12355cc0c098f2bd4df3b31dd5232d92e7caf
vertesy <a.vertesy@hubrecht.eu>
2016-07-04 13:00:43 +0200

    bash_profile entries for copasi multiplexing

---
+++ bash/COPASI_Multiplexing/Add_to_bash_profile.sh
@@ -0,0 +1,8 @@
+
+# to edit wrapper scripts quick
+alias 1copa='nano /home/hub_oudenaarden/avertesy/bin/COPASI/Scripts/01_submitter.sh'
+alias 2copa='rsub /home/hub_oudenaarden/avertesy/bin/COPASI/Scripts/02_output_merger.sh'
+
+# to execute scripts by their name from anywhere
+export PATH=/home/hub_oudenaarden/avertesy/bin/COPASI/Scripts/:$PATH
+export PATH=/home/hub_oudenaarden/avertesy/bin/COPASI/COPASI-4.16.104-Linux-64bit/bin/:$PATH
