78cca13343ee9ec565ad6e39052092cbb4260481
vertesy <a.vertesy@hubrecht.eu>
2016-08-08 16:35:46 +0200

    seed search for tSNE

---
+++ R/tSNE_seed_search.R
@@ -0,0 +1,19 @@
+
+# source("/Users/abelvertesy/Github_repos/TheCorvinas/R/tSNE_seed_search.R")
+
+# try (source ('/Users/abelvertesy/Github_repos/TheCorvinas/R/CodeAndRoll.R'),silent= F)
+
+OutDir = kollapse("/Users/abelvertesy/Google_Drive/Spermatogenesis_Data/sp1_analysis_new/tSNE_seed_Search_",signif(runif(1))) 
+setup_MarkdownReports(OutDir = OutDir)
+
+seedz =  c(211, -211, iround (runif(50, min = -10000, max = 10000) ));seedz
+write.simple.vec(seedz)
+
+s=1
+for (s in 1:seedz) {
+  sx = seedz[s]
+  sc <- comptsne2(sc,rseed=sx)
+  plottsne2(sc, final=F)
+  wplot_save_this(plotname = kollapse("tSNE_seed_",sx)) 
+}
+
