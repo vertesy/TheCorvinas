f81efc03068ef05d173523fb7584dc698094ebb2
vertesy <a.vertesy@hubrecht.eu>
2016-10-27 19:27:43 +0200

    Now uses 2 python scripts instead Dominic's perl scripts; Help reformatted

--- Python/MapAndGo/MapAndGo2.py
+++ Python/MapAndGo/MapAndGo2.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python
-# ## Usage
-# - Put the raw sequence data in a folder, put this script to folder where you store your scripts (e.g.: user/bin), and make it executable `chmod +X path/to/MapAndGo.py`
-# - The files should contain the minimum name of *__L00*_R*_001.fastq (that is Illumina NextSeq's default output: R for read 1 or 2, L for one of the 4 lanes)
-# - Test it by running in bash: `python path/to/MapAndGo.py -help`
-# - Run it with your data: python `path/to/MapAndGo.py MapAndGo.py -ref=human -bar=cel-seq_barcodes.csv -email=x.y@hubrecht.eu`
-# - Original author: Thom de Hoog, van Oudenaarden group, 02-03-2015
-# - Modified and maintained by Abel Vertesy, van Oudenaarden group, 16-08-2016
+### This script automates the mapping procedure
+### Put the raw sequence data in a folder, with this script
+### The files should contain the minimum name of *_L00*_R*_001.fastq
+### Run it with -> python MapAndGo.py
+### For usage run -> python MapAndGo.py -help
+# - Authors: Thom de Hoog and Abel Vertesy van Oudenaarden group, 02-03-2015
+# - Maintained by Abel Vertesy, van Oudenaarden group, 16-10-2016
 
 import glob
 import sys
 import os
 
-#Default parameters
-default_email = "<none>"
-default_bar = "/hpc/hub_oudenaarden/data/cel-seq_barcodes.csv"
-default_bash_out = os.getcwd()+'/bash_files'
-default_cat_out = os.getcwd()+'/cat_files'
-default_counts_out = os.getcwd()+'/count_files'
-default_map_out = os.getcwd()+'/map_files'
-default_logfiles_out = os.getcwd()+'/logs_and_errors'
+# Default parameters
+def_email = "<none>"
+def_CelSeqPrimerVersion = 1
+def_MaxHammingDist = 1
+def_ScriptsFolder = "/hpc/hub_oudenaarden/MapAndGo2/"
+def_BWA_Folder = "/hpc/hub_oudenaarden/bin/software/bwa-0.7.10/"
+def_bar = "/home/hub_oudenaarden/avertesy/var/"
+def_bash_out = os.getcwd()+'/bash_files'
+def_cat_out = os.getcwd()+'/cat_files'
+def_counts_out = os.getcwd()+'/count_files'
+def_map_out = os.getcwd()+'/map_files'
+def_logfiles_out = os.getcwd()+'/logs_and_errors'
 
-default_refseq_human = "/hpc/hub_oudenaarden/gene_models/human_gene_models/hg19_RefSeq_genes_clean_ERCC92_polyA_10_masked.fa"
-default_refseq_mouse = "/hpc/hub_oudenaarden/gene_models/mouse_gene_models/mm10_RefSeq_genes_clean_ERCC92_polyA_10_masked.fa"
-default_refseq_zebrafish = "/hpc/hub_oudenaarden/gene_models/zebrafish_gene_models/Danio_rerio_Zv9_ens74_extended3_genes_ERCC92.fa"
-default_refseq_elegans = "/hpc/hub_oudenaarden/gene_models/cel_gene_models/Aggregate_1003_genes_sorted_oriented_ERCC92.fa"
-default_refseq_briggsae = "/hpc/hub_oudenaarden/gene_models/cbr_gene_models/cb3_transcriptome_ERCC92.fa"
+def_refseq_human = "/hpc/hub_oudenaarden/gene_models/human_gene_models/hg19_RefSeq_genes_clean_ERCC92_polyA_10_masked.fa"
+def_refseq_mouse = "/hpc/hub_oudenaarden/gene_models/mouse_gene_models/mm10_RefSeq_genes_clean_ERCC92_polyA_10_masked.fa"
+def_refseq_zebrafish = "/hpc/hub_oudenaarden/gene_models/zebrafish_gene_models/Danio_rerio_Zv9_ens74_extended3_genes_ERCC92.fa"
+def_refseq_elegans = "/hpc/hub_oudenaarden/gene_models/cel_gene_models/Aggregate_1003_genes_sorted_oriented_ERCC92.fa"
+def_refseq_briggsae = "/hpc/hub_oudenaarden/gene_models/cbr_gene_models/cb3_transcriptome_ERCC92.fa"
 
-default_qsub = "no"
-default_mem = "5"
-default_time = "24:00:00"
-default_core = "8"
-default_unzip = "yes"
-default_zip = "no"
+def_qsub = "no"
+def_mem = "5"
+def_time = "12:00:00"
+def_core = "4"
+def_unzip = "yes"
+def_zip = "no"
 
 #Define necessary variables
 argv_imput = {}
-argv_valid = ["-help", "-email", "-bar", "-ref", "-mem", "-time", "-qsub", "-unzip", "-zip", "-bash_out", "-counts_out", "-cat_out", "-map_out", "-logs_out"]
+argv_valid = ["-help", "-email", "-qsub", "-bar", "-ref", "-mem", "-time", "-CelSeqPrimerVersion", "-MaxHammingDist", "-ScriptsFolder", "-BWA_Folder", "-unzip", "-zip", "-bash_out", "-counts_out", "-cat_out", "-map_out", "-logs_out"]
 bash_file_name = []
 do_break = False
 files = []
@@ -43,33 +47,39 @@ options = ["yes","no"]
 params = {}
 
 #Check if required default input is valid
-if not default_qsub.lower() in options:
+if not def_qsub.lower() in options:
 		sys.exit("\nThe default for the argument -qsub= is not supported!\n")
-if not default_unzip.lower() in options:
+if not def_unzip.lower() in options:
 		sys.exit("\nThe default for the argument -unzip= is not supported!\n")
-if not default_zip.lower() in options:
+if not def_zip.lower() in options:
 		sys.exit("\nThe default for the argument -zip= is not supported!\n")
 
-#Make help file
-line1 ="\nThe script automates the mapping procedure by making bash files containing the cat, do_mapping_strand.pl and extract_counts_rb.pl commands. \nIt works with .fastq files in the current working directory that have the minimal name of *_L00*_R*_*.fastq, \nand can submit them to the desired queue on the HPC. The following options are available for use:"
-line2 ="\n\n-qsub= \n\n\tThis determines if the generated bash file will be submitted on the queue. 	\n\tOptions: \"yes,\"no\". Default: \"no\"."
-line4 ="\n\n-ref= \n\n\tThis specifies the refseq file to be used for the do_mappings_strand.pl command. \n\tOptions: [refseq path] or, \n\t\t \"human\" which uses: " + default_refseq_human + ", \n\t\t \"mouse\" which uses: " + default_refseq_mouse + ", \n\t\t \"zebrafish\" which uses: " + default_refseq_zebrafish + " \n\tAlso C elegans and briggsae default gene models are present under these names. Default: \"mouse\"."
-line5 ="\n\n-bar= \n\n\tThis specifies the barcode file to be used for the extract_counts_rb.pl command. \n\tOptions: [barcode path]. Default: " + default_bar + "."
-line6 ="\n\n-cat_out= \n\n\tThis specifies the output folder for the cat command and makes the directory if it did not exist yet.\n\tOptions: [folder name]. Default: current working directory."
-line7 ="\n\n-bash_out= \n\n\tThis specifies the output folder for the bash files and makes the directory if it did not exist yet.\n\tOptions: [folder name]. Default: current working directory."
-line8 ="\n\n-map_out= \n\n\tThis specifies the output folder for the do_mapping.pl command and makes the directory if it did not exist yet.\n\tOptions: [folder name]. Default: current working directory."
-line9 ="\n\n-counts_out= \n\n\tThis specifies the output folder for the extract_counts.pl command and makes the directory if it did not exist yet.\n\tOptions: [folder name]. Default: current working directory."
-line10 ="\n\n-email= \n\n\tThis specifies the email address to be used for the do_mapping.pl command. \n\tOption: [email address]. Default: no email."
-line11 =" \n\n-unzip= \n\n\tThis determines if .fastq files to use are zipped and need to be unzipped. Options: \"yes\",\"no\". Default: \"no\"."
-line12 ="\n\n-zip= \n\n\tThis determines if the used .fastq files should be zipped after mapping. Options: \"yes\",\"no\". Default: \"no\"."
-line13 ="\n\n-help \n\n\tShows help!"
-line14 ="\n\nSummary of the defaults: \n\n\t"
-line15 ="\n\nExample: MapAndGo.py -qsub=no -ref=human -bar=cel-seq_barcodes.csv -email=x.y@hubrecht.eu"
-line16 ="\n\nImportant: Be careful to check the bash files!\n"
-help_text = line1 + line2 + line4 + line5 + line6 + line7 + line8 + line9 + line10 + line11 + line12 + line13 + line14 + line15 + line16
+# Make help file
+help_text = ( "\nThe script automates the mapping procedure by making bash files calling the cat, Concatenator.CELseq.py, bwa mem and the Tablator.CELseq.py scrips. \nIt works with .fastq files in the current working directory that have the minimal name of *_L00*_R*_*.fastq, \nand can submit them to the desired queue on the HPC. The following options are available for use:" +
+"\n\n-help \n\n\tShows help!" +
+"\n\n-qsub= \n\n\tThis determines if the generated bash file will be submitted on the queue. 	\n\tOptions: \"yes,\"no\". Default: \"no\"." +
+"\n\n-email= \n\n\tThis specifies the email address to be used for the do_mapping.pl command. \n\tOption: [email address]. Default: no email." +
+"\n\n-ref= \n\n\tThis specifies the refseq file to be used for the do_mappings_strand.pl command. \n\tOptions: [refseq path] or, \n\t\t \"human\" which uses: " + def_refseq_human + ", \n\t\t \"mouse\" which uses: " + def_refseq_mouse + ", \n\t\t \"zebrafish\" which uses: " + def_refseq_zebrafish + " \n\tAlso C elegans and briggsae default gene models are present under these names. Default: \"NONE\"." +
+# "\n\n-bar= \n\n\tThis specifies the barcode file to be used for the extract_counts_rb.pl command. \n\tOptions: [barcode path]. Default: " + def_bar + "." +
+"\n\n-CelSeqPrimerVersion= \n\n\t Using CELseq1 means read 1 starts with 8n cell barcode (cbc) + 4n UMI. CELseq2 means 6n UMI + 8 cbc \n\tOptions: [1 or 2]. Default: 1" +
+"\n\n-MaxHammingDist= \n\n\t How many mismatches are allowed in a cell barcode. If there is a single barcode maximum MaxHammingDist away, it counts it. \n\tOptions: [integer<4]. Default: 1" +
+"\n\n-ScriptsFolder= \n\n\t  Where are the MapAndGo2 sripts located.\n\tOptions: [folder name]. Default: /hpc/hub_oudenaarden/MapAndGo2/" +
+"\n\n-BWA_Folder= \n\n\t Where is your version of BWA located\n\tOptions: [folder name]. Default: /hpc/hub_oudenaarden/bin/software/bwa-0.7.10/" +
+"\n\n-mem= \n\n\t Memory to reserve \n\tOptions: [GB, integer]. Default: 5" +
+"\n\n-time= \n\n\t Max estimated running time. If it runs longer, HPC kills the job \n\tOptions: [hours, HH:MM:SS]. Default: 12:00:00" +
+"\n\n-core= \n\n\t Number of cores reserved. The more cores you reserve the longer your job will wait to start, but then the faster it runs. \n\tOptions: [integer]. Default: 4" +
+"\n\n-cat_out= \n\n\tThis specifies the output folder for the cat command and makes the directory if it did not exist yet.\n\tOptions: [folder name]. Default: current working directory." +
+"\n\n-bash_out= \n\n\tThis specifies the output folder for the bash files and makes the directory if it did not exist yet.\n\tOptions: [folder name]. Default: current working directory." +
+"\n\n-map_out= \n\n\tThis specifies the output folder for the do_mapping.pl command and makes the directory if it did not exist yet.\n\tOptions: [folder name]. Default: current working directory." +
+"\n\n-counts_out= \n\n\tThis specifies the output folder for the extract_counts.pl command and makes the directory if it did not exist yet.\n\tOptions: [folder name]. Default: current working directory." +
+"\n\n-def_logfiles_out= \n\n\tThis specifies the output folder for Logfiles.\n\tOptions: [folder name]. Default: current working directory." +
+"\n\n-unzip= \n\n\tThis determines if .fastq files to use are zipped and need to be unzipped. Options: \"yes\",\"no\". Default: \"no\"." +
+"\n\n-zip= \n\n\tThis determines if the used .fastq files should be zipped after mapping. Options: \"yes\",\"no\". Default: \"no\"." +
+"\n\nThe Defaults: \n\n\t" +
+"\n\nExample: MapAndGo.py -email = <NONE> -CelSeqPrimerVersion = 1 -MaxHammingDist = 1 -bash_out = os.getcwd()+'/bash_files' -cat_out = os.getcwd()+'/cat_files' -counts_out = os.getcwd()+'/count_files' -map_out = os.getcwd()+'/map_files' -logfiles_out = os.getcwd()+'/logs_and_errors' -qsub = no  -mem = 5  -time = 12:00:00  -core = 4  -unzip = yes  -zip = no  " +
+"\n\nImportant: Be careful to check the bash files!\n" )
 
-
-#Readout the command line arguments
+# Readout the command line arguments ----
 for i in range(1, len(sys.argv)):
 	if sys.argv[i].lower() == '-help':
 		sys.exit(help_text)
@@ -87,18 +97,22 @@ for i in range(1, len(sys.argv)):
 if do_break:
 	sys.exit(break_message + "\nFor help type: MapAndGo.py -help\n")
 
+# Currently fixed (ignores what you put in)
+params["-MaxHammingDist"] = def_MaxHammingDist
+params["-ScriptsFolder"] = def_ScriptsFolder
+params["-BWA_Folder"] = def_BWA_Folder
 
-#Process parameter for barcode file
+# Process parameter for barcode file
 if "-bar" in argv_imput:
 	if  os.path.isfile(argv_imput["-bar"]):
 		params["-bar"] = argv_imput["-bar"]
 	else:
 		sys.exit("\nThe barcode file does not exist!\n")
 else:
-	if not os.path.isfile(default_bar):
+	if not os.path.isfile(def_bar):
 		sys.exit("\nThe default barcodes file does not exist!\n")
 	else:
-		params["-bar"] =  default_bar
+params["-bar"] =  def_bar
 
 #Process parameter for bash_out
 if "-bash_out" in argv_imput:
@@ -108,9 +122,9 @@ if "-bash_out" in argv_imput:
 	params["-bash_out"] = os.getcwd() + "/" + argv_imput["-bash_out"]
 
 else:
-	if default_bash_out[(len(default_bash_out)-1)] == "/":
-		default_bash_out = default_bash_out[0:(len(default_bash_out)-1)]
-	params["-bash_out"] = default_bash_out
+	if def_bash_out[(len(def_bash_out)-1)] == "/":
+		def_bash_out = def_bash_out[0:(len(def_bash_out)-1)]
+	params["-bash_out"] = def_bash_out
 
 #Process parameter for cat_out
 if "-cat_out" in argv_imput:
@@ -120,9 +134,9 @@ if "-cat_out" in argv_imput:
 		params["-cat_out"] = os.getcwd() + "/" + argv_imput["-cat_out"]
 
 else:
-		if default_cat_out[(len(default_cat_out)-1)] == "/":
-				default_cat_out = default_cat_out[0:(len(default_cat_out)-1)]
-		params["-cat_out"] = default_cat_out
+		if def_cat_out[(len(def_cat_out)-1)] == "/":
+				def_cat_out = def_cat_out[0:(len(def_cat_out)-1)]
+		params["-cat_out"] = def_cat_out
 
 #Process parameter for counts_out
 if "-counts_out" in argv_imput:
@@ -132,9 +146,9 @@ if "-counts_out" in argv_imput:
 	params["-counts_out"] = os.getcwd() + "/" + argv_imput["-counts_out"]
 
 else:
-	if default_counts_out[(len(default_counts_out)-1)] == "/":
-		default_counts_out = default_counts_out[0:(len(default_counts_out)-1)]
-	params["-counts_out"] = default_counts_out
+	if def_counts_out[(len(def_counts_out)-1)] == "/":
+		def_counts_out = def_counts_out[0:(len(def_counts_out)-1)]
+	params["-counts_out"] = def_counts_out
 
 #Process parameter for map_out
 if "-map_out" in argv_imput:
@@ -144,9 +158,9 @@ if "-map_out" in argv_imput:
 	params["-map_out"] = os.getcwd() + "/" +  argv_imput["-map_out"]
 
 else:
-	if default_map_out[(len(default_map_out)-1)] == "/":
-		default_map_out = default_map_out[0:(len(default_map_out)-1)]
-	params["-map_out"] = default_map_out
+	if def_map_out[(len(def_map_out)-1)] == "/":
+		def_map_out = def_map_out[0:(len(def_map_out)-1)]
+	params["-map_out"] = def_map_out
 
 if "-logs_out" in argv_imput:
 	logs_out = argv_imput["-logs_out"]
@@ -155,9 +169,9 @@ if "-logs_out" in argv_imput:
 	params["-logs_out"] = os.getcwd() + "/" +  argv_imput["-logs_out"]
 
 else:
-	if default_logfiles_out[(len(default_logfiles_out)-1)] == "/":
-		default_logfiles_out = default_logfiles_out[0:(len(default_logfiles_out)-1)]
-	params["-logs_out"] = default_logfiles_out
+	if def_logfiles_out[(len(def_logfiles_out)-1)] == "/":
+		def_logfiles_out = def_logfiles_out[0:(len(def_logfiles_out)-1)]
+	params["-logs_out"] = def_logfiles_out
 
 #Check if defaults folders exists
 if not os.path.isdir(params["-bash_out"]):
@@ -174,32 +188,32 @@ if not os.path.isdir(params["-logs_out"]):
 #Set parameter for refseq file ------------------------------------------------------------------------------------------------------------
 if "-ref" in argv_imput:
 	if argv_imput["-ref"] == "human":
-		if os.path.isfile(default_refseq_human):
-			params["-ref"] = default_refseq_human
+		if os.path.isfile(def_refseq_human):
+			params["-ref"] = def_refseq_human
 		else:
 			sys.exit("\nThe default human refseq file is not found! Check if the file exist and if the right path is given!\n")
 
 	elif argv_imput["-ref"] == "mouse":
-		if os.path.isfile(default_refseq_mouse):
-			params["-ref"] = default_refseq_mouse
+		if os.path.isfile(def_refseq_mouse):
+			params["-ref"] = def_refseq_mouse
 		else:
 			sys.exit("\nThe default mouse refseq file is not found! Check if the file exist and if the right path is given!\n")
 
 	elif argv_imput["-ref"] == "zebrafish":
-		if os.path.isfile(default_refseq_zebrafish):
-			params["-ref"] = default_refseq_zebrafish
+		if os.path.isfile(def_refseq_zebrafish):
+			params["-ref"] = def_refseq_zebrafish
 		else:
 			sys.exit("\nThe default zebrafish refseq file is not found! Check if the file exist and if the right path is given!\n")
 
 	elif argv_imput["-ref"] == "elegans":
-		if os.path.isfile(default_refseq_elegans):
-			params["-ref"] = default_refseq_elegans
+		if os.path.isfile(def_refseq_elegans):
+			params["-ref"] = def_refseq_elegans
 		else:
 			sys.exit("\nThe default C. elegans refseq file is not found! Check if the file exist and if the right path is given!\n")
 
 	elif argv_imput["-ref"] == "briggsae":
-		if os.path.isfile(default_refseq_briggsae):
-			params["-ref"] = default_refseq_briggsae
+		if os.path.isfile(def_refseq_briggsae):
+			params["-ref"] = def_refseq_briggsae
 		else:
 			sys.exit("\nThe default C. briggsae refseq file is not found! Check if the file exist and if the right path is given!\n")
 
@@ -209,30 +223,27 @@ if "-ref" in argv_imput:
 		else:
 			sys.exit("\nThe refseq file is not found! Check if the file exist and if the right path is given! \n")
 else:
-	if not os.path.isfile(default_refseq_mouse):
-		sys.exit("\nThe default refseq file is not found! Check if the file exist and if the right path is given!\n")
-	else:
-		params["-ref"] =  default_refseq_mouse
+	print "Specify mapping reference in '-ref' ! Either write human, mouse, zebrafish, elegans, briggsae, or provide the path"
 
 #Set parameter for mem
 if "-mem" in argv_imput:
 	params["-mem"] = argv_imput["-mem"].lower()
 	# sys.exit("\nMem has to be a whole number denoting the gigabytes requested: 10 \n")
 else:
-	params["-mem"] = "#$ -l h_vmem=" + default_mem + "G"
+	params["-mem"] = "#$ -l h_vmem=" + def_mem + "G"
 
 #Set parameter for time
 if "-time" in argv_imput:
 	params["-time"] = argv_imput["-time"].lower()
 	# sys.exit("\nTime has to be in this format: 12:00:00\n")
 else:
-	params["-time"] = "#$ -l h_rt=" + default_time
+	params["-time"] = "#$ -l h_rt=" + def_time
 
 # Set parameter for threads
 if "-cores" in argv_imput:
 	params["-cores"] = argv_imput["-cores"]
 else:
-	params["-cores"] = default_core
+	params["-cores"] = def_core
 
 #Set parameter for unzip
 if "-unzip" in argv_imput:
@@ -241,7 +252,7 @@ if "-unzip" in argv_imput:
 		else:
 				sys.exit("\nOnly \"yes\" and \"no\" are supported for -unzip\n")
 else:
-		params["-unzip"] = default_unzip
+		params["-unzip"] = def_unzip
 
 #Set parameter for zip
 if "-zip" in argv_imput:
@@ -250,15 +261,15 @@ if "-zip" in argv_imput:
 	else:
 		sys.exit("\nOnly \"yes\" and \"no\" are supported for -zip\n")
 else:
-	params["-zip"] = default_zip
+	params["-zip"] = def_zip
 
 #Process parameter for email
 if "-email" in argv_imput:
 	params["-email"] = "\n#$ -M " + argv_imput["-email"]
 	params["-email_fb"] = argv_imput["-email"]
 else:
-	params["-email"] = "\n#$ -M " + default_email
-	params["-email_fb"] = default_email
+	params["-email"] = "\n#$ -M " + def_email
+	params["-email_fb"] = def_email
 
 #Set parameter for qsub
 if "-qsub" in argv_imput:
@@ -267,7 +278,7 @@ if "-qsub" in argv_imput:
 	else:
 		sys.exit("\nOnly \"yes\" and \"no\" are supported for -qsub\n")
 else:
-	params["-qsub"] = default_qsub
+	params["-qsub"] = def_qsub
 
 # Head of the bash files ------------------------------------------------------------------------------------------------------------------------------------------------
 head_bash = "#! /bin/bash" + "\n#$ -cwd \n#$ -V \n" + params["-time"] +"\n" + params["-mem"] + params["-email"] +" \n#$ -m beas \n#$ -pe threaded "  + params["-cores"]
