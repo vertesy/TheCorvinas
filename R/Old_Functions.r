eabd417bb480c7b55b8b462576a019140faf4969
vertesy <a.vertesy@hubrecht.eu>
2017-08-15 13:09:45 +0200

    old functions removed from CodeAndRoll

--- R/Old_Functions.r
+++ R/Old_Functions.r
@@ -1,6 +1,5 @@
-
-## _Old_Functions.r
-# source("~/Dokumentumok/Tanulas/PhD/AvanO/Data_analysis/X_inact/Scripts_Xreact/zz_Old_versions/_Old_Functions.r")
+## _Old_Functions that are kept for reference!
+# source("~/Github_repos/TheCorvinas/R/Old_Functions.r")
 
 
 # old <- `[`
@@ -197,3 +196,302 @@ whist.nonCol <-  function(variable, plotname = substitute(variable), ..., w=7, h
 	hist (variable, ..., main=plotname)
 	dev.copy2pdf (file=FnP, width=w, height=h )
 }
+
+
+
+# 15 August 2017 (Tuesday) 13:05  -------------------------------------------------------
+
+#' #' qlegend
+#' #' # Quickly add a legend, and save the plot immediately
+#' #'
+#' #' @param NamedColorVec A vector defining the Color of the boxes, while its names define the labels.
+#' #' @param poz 1:4 corresponding to "topleft","topright", "bottomright", "bottomleft"
+#' #' @param ... Additional parameters for legend()
+#' #' @param w_ Width of the saved pdf image, in inches.
+#' #' @param h_ Height of the saved pdf image, in inches.
+#' #' @param bty The type of box to be drawn around the legend. The allowed values are "o" (the default) and "n".
+#' #' @param OverwritePrevPDF Save the plot immediately with the same name the last wplot* function made (It is stored in plotnameLastPlot variable).
+#' #' @export
+#' #'
+#' #' @examples ccc = as.factor.numeric(c(6,7,8,7,7,6,6,8))  ; qlegend(ccc) # Uses as.factor.numeric() from Github / Vertesy / TheCorvinas
+#'
+#' qlegend <- function(NamedColorVec, poz=3, ..., w_=7, h_=w_, bty = "n", OverwritePrevPDF =T) {
+#'   pozz = translate(poz, oldvalues = 1:4, newvalues = c("topleft","topright", "bottomright", "bottomleft"))
+#'   fill_ = getCategories(NamedColorVec)
+#'   legend(x=pozz, legend=names(fill_), fill=fill_, ..., bty=bty)
+#'   if (OverwritePrevPDF) {   wplot_save_this(plotname = plotnameLastPlot, w= w_, h = h_)  }
+#' }
+#'
+
+
+# cormethod = "spearman"
+# panel.cor <- function(x, y, digits=2, prefix="", cex.cor, method = cormethod) { # A function to display correlation values for pairs() function. Default is pearson correlation, that can be set to  "kendall" or "spearman".
+#   usr <- par("usr"); on.exit(par(usr))
+#   par(usr = c(0, 1, 0, 1))
+#   r <- abs(cor(x, y, method = method))
+#   txt <- format(c(r, 0.123456789), digits=digits)[1]
+#   txt <- paste(prefix, txt, sep="")
+#   if(missing(cex.cor)) cex <- 0.8/strwidth(txt)
+#
+#   test <- cor.test(x,y)
+#   # borrowed from printCoefmat
+#   Signif <- symnum(test$p.value, corr = FALSE, na = FALSE,
+#                    cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+#                    symbols = c("***", "**", "*", ".", " "))
+#
+#   text(0.5, 0.5, txt, cex = cex * r)
+#   text(.8, .8, Signif, cex=cex, col=2)
+# }
+
+
+# http://stackoverflow.com/questions/20127282/r-color-scatterplot-points-by-col-value-with-legend
+# scatter_fill <- function (x, y, color, xlim=range(x), ylim=range(y), zlim=range(color),
+#                           nlevels = 20, plot.title, plot.axes, pch=21, cex=1,
+#                           key.title, key.axes, asp = NA, xaxs = "i", yaxs = "i", las = 1,
+#                           axes = TRUE, frame.plot = axes, ...) {
+#   mar.orig <- (par.orig <- par(c("mar", "las", "mfrow")))$mar
+#   on.exit(par(par.orig))
+#   w <- (3 + mar.orig[2L]) * par("csi") * 2.54
+#   layout(matrix(c(2, 1), ncol = 2L), widths = c(1, lcm(w)))
+#   par(las = las)
+#   mar <- mar.orig
+#   mar[4L] <- mar[2L]
+#   mar[2L] <- 1
+#   par(mar = mar)
+#
+#   # choose colors to interpolate
+#   levels <- seq(zlim[1], zlim[2], length.out = nlevels)
+#   col <- colorRampPalette(c("red", "yellow", "dark green"))(nlevels)
+#   colz <- col[cut(color, nlevels)]
+#
+#   plot.new()
+#   plot.window(xlim = c(0, 1), ylim = range(levels), xaxs = "i", yaxs = "i")
+#
+#   rect(0, levels[-length(levels)], 1, levels[-1L], col=col, border=col)
+#   if (missing(key.axes)) {if (axes){axis(4)}}
+#   else key.axes
+#   box()
+#   if (!missing(key.title))
+#     key.title
+#   mar <- mar.orig
+#   mar[4L] <- 1
+#   par(mar = mar)
+#
+#   # points
+#   plot(x, y, type = "n", xaxt='n', yaxt='n', xlab="", ylab="", xlim=xlim, ylim=ylim, bty="n")
+#   points(x, y, bg = colz, xaxt='n', yaxt='n', xlab="", ylab="", bty="n", pch=pch,...)
+#
+#   ## options to make mapping more customizable
+#   if (missing(plot.axes)) {
+#     if (axes) {
+#       title(main = "", xlab = "", ylab = "")
+#       Axis(x, side = 1)
+#       Axis(y, side = 2)
+#     }
+#   }
+#   else plot.axes
+#   if (frame.plot)
+#     box()
+#   if (missing(plot.title))
+#     title(...)
+#   else plot.title
+#   invisible()
+# }
+
+
+# annot_col.create <- function(df, annot_vec, annot_names=NULL) { # Auxiliary function for pheatmap. Prepares the 2 variables needed for "annotation_col" and "annotation_colors" in pheatmap
+#   stopifnot( l(annot_vec) == dim(df)[2] )
+#   print(substitute(annot_vec))
+#   df = as.data.frame(annot_vec)
+#   if (!is.null(annot_names)) {  stopifnot(length(annot_vec) == length(annot_names));
+#     colnames(df) = annot_names
+#   } else {    colnames(df) =  substitute(annot_vec)    }
+#
+#   df[,1] = as.character(df[,1])
+#   assign(x = "annot", value = df, envir = .GlobalEnv)
+#
+#   xx = list(annot_vec = val2col(annot_vec[!duplicated(annot_vec)]))
+#   if (!is.null(annot_names)) {    stopifnot(length(annot_col) == length(annot_names));
+#     names(xx) = annot_names  }
+#   else {    names(xx) = substitute(annot_vec)  }
+#
+#   assign(x = "annot_col", value = xx, envir = .GlobalEnv)
+#   print("annot and annot_col variables are created. Use: pheatmap(..., annotation_col = annot, annotation_colors = annot_col)")
+# }
+# # annot_col.create
+
+# JUNKYARD --------------------------------------------------------------------------------
+
+# select.rows <- function(df, RowIndices ) {
+#   true_rownames = intersect(rownames(df), RowIndices)
+#   NotFound = setdiff(RowIndices, rownames(df))
+#   if (l(NotFound)) { any_print(l(NotFound), "Row Indices Not Found:", head(NotFound), "...     Rows found:", l(true_rownames))  } #if
+#   return(df[ true_rownames, ])
+# }
+
+# corrr::correlate(mtcars[, 1:4], method = "pearson") %>%
+#   corrr::rearrange(absolute=F) %>% #  rearrange cols and rows
+#   corrr::shave()%>% # shave upper triangle
+#   corrr::rplot(print_cor = T,legend = T)# dot plot
+#
+# corrr::correlate(mtcars[, 1:4], method = "pearson") %>% network_plot()
+
+
+# ## A largish data set
+# n <- 10000
+# x1  <- matrix(rnorm(n), ncol = 2)
+# x2  <- matrix(rnorm(n, mean = 3, sd = 1.5), ncol = 2)
+# x   <- rbind(x1, x2)
+# oldpar <- par(mfrow = c(2, 2))
+# smoothScatter(x, nrpoints = 0)
+# dim(x)
+# l(x)
+
+
+# # http://stackoverflow.com/questions/14271584/r-legend-for-color-density-scatterplot-produced-using-smoothscatter
+# install.packages("fields")
+# fudgeit <- function(){
+#   xm <- get('xm', envir = parent.frame(1))
+#   ym <- get('ym', envir = parent.frame(1))
+#   z  <- get('dens', envir = parent.frame(1))
+#   colramp <- get('colramp', parent.frame(1))
+#   fields::image.plot(xm,ym,z, col = colramp(256), legend.only = T, add =F)
+# }
+# #
+# par(mar = c(5,4,4,5) + .1)
+# smoothScatter(x, nrpoints = 0, postPlotHook = fudgeit)
+
+
+# wvenn <- function (yalist, imagetype = "png", alpha = .5, fill = 1:length(yalist), ..., w = 7, h = 7, mdlink = F, plotname = substitute(yalist)) {
+#   if (!require("VennDiagram")) { print("Please install VennDiagram: install.packages('VennDiagram')") }
+#   fname = kollapse(plotname, ".", imagetype, print = F)
+#   LsLen = length(yalist)
+#   if(length(names(yalist)) < LsLen) { names(yalist) =1:LsLen; print("List elements had no names.") }
+#   print(names(yalist))
+#
+#   filename = kollapse(OutDir,"/", fname, print = F)
+#   subt = kollapse("Total = ", length(unique(unlist(yalist))), " elements in total.", print = F)
+#   venn.diagram(x = yalist, imagetype = imagetype, filename = filename, main = plotname, ... ,
+#                sub = subt, fill = fill, alpha = alpha, sub.cex = .75, main.cex = 2)
+#   if (mdlink) {
+#     llogit(MarkDown_ImgLink_formatter(fname))
+#     if (exists("png4Github") & png4Github == T) { llogit(MarkDown_ImgLink_formatter(paste0("Reports/", fname) ) )	}
+#   }
+# }
+
+# wLinRegression <- function(DF, coeff = c("pearson", "spearman", "r2")[3], textlocation = "topleft", savefile =T, ...) { # Add linear regression, and descriptors to line to your scatter plot. Provide the same dataframe as you provided to wplot() before you called this function
+#   print(coeff)
+#   regression <- lm(DF[,2] ~ DF[,1])
+#   abline(regression, ...)
+#   legendText = NULL
+#   if ( "pearson" %in% coeff) {    dispCoeff = iround(cor(DF[,2], DF[,1], method = "pearson"))
+#   legendText  =  c(legendText, paste0("Pearson c.c.: ", dispCoeff))  }
+#   if ("spearman" %in% coeff) {    dispCoeff = iround(cor(DF[,2], DF[,1], method = "spearman"))
+#   legendText = c(legendText, paste0("Spearman c.c.: ", dispCoeff))  }
+#   if ("r2" %in% coeff) {          r2 = iround(summary(regression)$r.squared)
+#   legendText = c(legendText, paste0("R^2: ", r2))  }
+#   print(legendText)
+#   if (length(coeff)==1 & "r2" == coeff[1]) {  legend(textlocation, legend = superscript_in_plots(prefix = "R", sup = "2",suffix = paste0(": ", r2)) , bty="n")
+#   } else {                                    legend(textlocation, legend = legendText , bty="n") }
+#   if(savefile){   wplot_save_this(plotname = plotnameLastPlot) }
+# }
+
+
+
+
+#' #' wpie
+#' #'
+#' #' Create and save pie charts as .pdf, in "OutDir". If mdlink =T, it inserts a .pdf and a .png link in the markdown report, set by "path_of_report". The .png version is not created, only the link is put in place, not to overwrite previous versions.
+#' #' @param variable The variable to plot.
+#' #' @param ... Pass any other parameter of the corresponding plotting function (most of them should work).
+#' #' @param percentage Display percentage instead of counts. TRUE by default.
+#' #' @param both_pc_and_value Report both percentage AND number.
+#' #' @param plotname Title of the plot (main parameter) and also the name of the file.
+#' #' @param col Fill color. Defined by rich colours by default
+#' #' @param savefile Save plot as pdf in OutDir, TRUE by default.
+#' #' @param w Width of the saved pdf image, in inches.
+#' #' @param h Height of the saved pdf image, in inches.
+#' #' @param mdlink Insert a .pdf and a .png image link in the markdown report, set by "path_of_report".
+#' #' @examples wpie (variable =  , ... =  , percentage = TRUE, plotname = substitute(variable), w = 7, h = 7, mdlink = F)
+#' #' @export
+#'
+#' wpie <-function (variable, ..., percentage = TRUE, both_pc_and_value=F, plotname = substitute(variable), col = gplots::rich.colors(length(variable)), savefile = T, w = 7, h = 7, mdlink = F) {
+#'   if (!require("gplots")) { print("Please install gplots: install.packages('gplots')") }
+#'   fname = kollapse(plotname, ".pie")
+#'   subt = kollapse("Total = ", sum(variable), print = F)
+#'   if (percentage) {	labs <- paste("(", names(variable), ")", "\n", percentage_formatter(variable/sum(variable)), sep = "")
+#'   if (both_pc_and_value) { labs <- paste("(", names(variable), ")", "\n", percentage_formatter(variable/sum(variable)),"\n", variable , sep = "")}
+#'   } else {	labs <- paste("(", names(variable), ")", "\n", variable, sep = "")	}
+#'   pie(variable, ..., main = plotname, sub = subt, clockwise = T, labels = labs, col = col )
+#'   if (savefile) { dev.copy2pdf(file = FnP_parser(fname, "pdf"), width = w, height = h, title = ttl_field()) }
+#'   if (mdlink) { MarkDown_Img_Logger_PDF_and_PNG(fname_wo_ext = fname) }
+#' }
+#'
+
+
+# wvioplot_list <-function (yalist, ..., coll = c(2:(length(yalist)+1)),
+#                           plotname = as.character(substitute(yalist)), sub = NULL, xlb = names(yalist), ylb = "", ylimm=F,
+#                           incrBottMarginBy = 0, tilted_text = F, yoffset=0, savefile = T, w = 7, h = 7, mdlink = F) {
+#   if (!require("vioplot")) { print("Please install vioplot: install.packages('vioplot')") }
+#   if (incrBottMarginBy) { .ParMarDefault <- par("mar"); 	par(mar=c(par("mar")[1]+incrBottMarginBy, par("mar")[2:4]) ) } 	# Tune the margin
+#   l_list = length(yalist)
+#   fname = kollapse(plotname, ".vioplot")
+#   if (length(coll) < l_list) { coll = rep(coll, l_list) }
+#   if (tilted_text) {	xlb = NA } else { xlb = names(yalist) }
+#   if (! (is.numeric(ylimm) & length(ylimm)==2)) { ylimm = range(unlist(yalist),na.rm = T)}
+#   plot(0, 0, type = "n", xlim = c(0.5, (l_list + 0.5)), ylim = ylimm, xaxt = "n", xlab = "",
+#        ylab = ylb, main = plotname, sub = sub)
+#   for (i in 1:l_list) {
+#     if( l(na.omit.strip(yalist[[i]])) ){
+#       vioplot::vioplot(na.omit(yalist[[i]]), ..., at = i, add = T, col = coll[i])
+#     }
+#   }
+#   axis(side = 1, at = 1:l_list, labels = xlb, las = 2)
+#   if (tilted_text) {
+#     text(x = 1:length(yalist), y = min(unlist(yalist))+yoffset, labels = names(yalist), xpd = TRUE, srt = 45)
+#   }
+#   if (savefile) { dev.copy2pdf(file = FnP_parser(fname, "pdf"), width = w, height = h, title = paste0(basename(fname), " by ", if (exists("scriptname")) scriptname else "Rscript")) }
+#   if (incrBottMarginBy) { par("mar" = .ParMarDefault )}
+#   assign("plotnameLastPlot", fname, envir = .GlobalEnv)
+#   if (mdlink) { MarkDown_Img_Logger_PDF_and_PNG(fname_wo_ext = fname) }
+# }
+
+
+# create_set_SubDir <-function (..., makeOutDirOrig=T, setDir=T) {
+#   NewOutDir = kollapse(OutDir,"/", ..., print = F)
+#   any_print("All files will be saved under 'NewOutDir': ", NewOutDir)
+#   if (!exists(NewOutDir)) {	dir.create(NewOutDir)	}
+#   if (setDir) {	setwd(NewOutDir)}
+#   if (makeOutDirOrig) {
+#     if (exists("OutDirOrig")) any_print("OutDirOrig was defined as:",OutDirOrig)
+#     any_print("OutDirOrig will be:", OutDir)
+#     assign("OutDirOrig", OutDir, envir = .GlobalEnv)
+#   } #if
+#
+#   assign("OutDir", NewOutDir, envir = .GlobalEnv)
+# }
+
+
+# wvenn <- function (yalist, imagetype = "png", alpha = .5, fill = 1:length(yalist), ..., w = 7, h = 7, mdlink = F) {
+#   if (!require("VennDiagram")) { print("Please install VennDiagram: install.packages('VennDiagram')") }
+#   fname = kollapse(substitute(yalist), ".", imagetype, print = F)
+#   filename = kollapse(OutDir,"/", fname, print = F)
+#   subt = kollapse("Total = ", length(unique(unlist(yalist))), " elements in total.", print = F)
+#   venn.diagram(x = yalist, imagetype = imagetype, filename = filename, main = substitute(yalist), ... ,
+#                sub = subt, fill = fill, alpha = alpha, sub.cex = .75, main.cex = 2)
+#   if (mdlink) {
+#     llogit(MarkDown_ImgLink_formatter(fname))
+#     if (exists("png4Github") & png4Github == T) { llogit(MarkDown_ImgLink_formatter(paste0("Reports/", fname) ) )	}
+#   }
+# }
+
+
+# ttl_field <- function (flname = basename(fname) ) { paste0(flname, " by ", if (exists("scriptname")) scriptname else "Rscript") }
+
+### THIS IS DUPLICATE OF THE ONE IN MD REPORTS
+# llwrite_list <- function(yalist) {
+#   for (e in 1:l(yalist)) {
+#     if (is.null( names(yalist) )) { llprint("#####",names(yalist)[e]) } else { llprint("#####", e)}
+#     print(yalist[e]); llogit("`", yalist[e], "`")
+#   }
+# }
