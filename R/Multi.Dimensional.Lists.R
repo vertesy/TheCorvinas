cea94845e485e39ad93dff0de5f5fcc3a1f06742
vertesy <a.vertesy@hubrecht.eu>
2018-05-24 22:13:08 +0200

    ...

--- R/Multi.Dimensional.Lists.R
+++ R/Multi.Dimensional.Lists.R
@@ -3,15 +3,20 @@
 # lapply(df.MapData.per.Worm, l)
 # idim(df.MapData.per.Worm)
 # xsapply(words[1:12], str_split, pattern=",")
+require(stringr)
+datMAT=matrix(data = words[1:12], nrow = 3)
 
-fred <- sapply(words[1:12], str_split, pattern="")
-fred <- as.list(1:12)
+fred <- sapply(datMAT, str_split, pattern="")
+# fred <- as.list(1:12)
 
 
-dim(fred) <- c(3, 4)
+dim(fred) <- c(3, 4); fred
 dimnames(fred) = list(letters[1:3], LETTERS[1:4])
 fred[['a','A']]
 
+lapply(fred,unlist)
+sapply(fred,unlist)[2,2]
+arr.of.lists.2.df(mdlapply(fred,l))
 
 
 copy.dimension.and.dimnames <- function(list.1D, obj.2D) {  # copy dimension and dimnames
