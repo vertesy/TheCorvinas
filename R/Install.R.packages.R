c70579f2fd7b51ddd97c59724710dca60e6da1be
vertesy <a.vertesy@hubrecht.eu>
2018-08-08 18:26:02 +0000

    ...

---
+++ R/Install.R.packages.R
@@ -0,0 +1,27 @@
+######################################################################
+# A collection of custom R functions
+######################################################################
+
+## Setup   -------------------------------------------------------------------------------------------------
+
+install.packages("devtools")
+install.packages("bookdown")
+install.packages("tidyverse")
+install.packages("sm")
+install.packages("gplots")
+install.packages("RColorBrewer")
+install.packages("colorRamps")
+install.packages("vioplot")
+install.packages("VennDiagram")
+install.packages("rmarkdown")
+install.packages("commonmark")
+install.packages("clipr")
+install.packages("data.table")
+install.packages("pheatmap")
+install.packages("revealjs")
+install.packages("readr")
+# install.packages("Seurat")
+# install.packages("robustbase")
+# devtools::install_github(repo = "vertesy/MarkdownReportsDev")
+devtools::install_github("csgillespie/roxygen2Comment")
+
