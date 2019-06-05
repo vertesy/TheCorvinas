60df500b7238babe43431dc23690cf56ba64b9a7
Abel Vertesy <vertesy@users.noreply.github.com>
2019-06-05 23:56:30 +0200

    update

--- R/Template.R
+++ R/Template.R
@@ -5,10 +5,9 @@
 # rm(list=ls(all.names = TRUE)); try(dev.off(), silent = T)
 
 # Functions ------------------------
-try (source ('~/Github_repos/TheCorvinas/R/CodeAndRoll.R'),silent= F)
-try (source("~/Github_repos/Spermatogenesis/Mapping/functions_sp.r") , silent= F)
-source("~/Github_repos/hmC/RaceID/RaceID2_class_ext.2016.05.10.R") ## load class definition and functions
-source ('~/Github_repos/TheCorvinas/R/DatabaseLinke.r')
+try (source ('~/GitHub/TheCorvinas/R/CodeAndRoll.R'),silent= F)
+require('MarkdownReportsDev')
+# source ('~/Github/TheCorvinas/R/DatabaseLinke.r')
 
 # Setup ------------------------
 OutDir = ""
@@ -18,6 +17,7 @@ OutDirOrig = OutDir
 # Metadata ------------------------
 
 # Parameters ------------------------
+
 
 # Read In ------------------------
 
