24a148ec3a1e02743efc104cddc600bb1b3f63a5
Abel Vertesy <vertesy@users.noreply.github.com>
2019-07-30 19:26:26 +0200

    Update Install.R.packages.R

--- R/Install.R.packages.R
+++ R/Install.R.packages.R
@@ -3,25 +3,32 @@
 ######################################################################
 
 ## Setup   -------------------------------------------------------------------------------------------------
-
-install.packages("devtools")
-install.packages("bookdown")
+install.packages("BiocManager")
 install.packages("tidyverse")
+install.packages("devtools")
+# install.packages("bookdown")
 install.packages("sm")
 install.packages("gplots")
+install.packages("gtools")
+
 install.packages("RColorBrewer")
 install.packages("colorRamps")
 install.packages("vioplot")
 install.packages("VennDiagram")
-install.packages("rmarkdown")
-install.packages("commonmark")
+# install.packages("rmarkdown")
+# install.packages("commonmark")
 install.packages("clipr")
 install.packages("data.table")
 install.packages("pheatmap")
-install.packages("revealjs")
+# install.packages("revealjs")
 install.packages("readr")
-# install.packages("Seurat")
+install.packages("Seurat")
 # install.packages("robustbase")
 # devtools::install_github(repo = "vertesy/MarkdownReportsDev")
-devtools::install_github("csgillespie/roxygen2Comment")
+# devtools::install_github("csgillespie/roxygen2Comment")
+
+
+
+require("devtools")
+devtools::install_github(repo = "vertesy/MarkdownReportsDev")
 
