715e1f8d209ac2d4bc710a264b546573898338de
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-12-03 14:52:40 +0100

    ...

--- R/Template.R
+++ R/Template.R
@@ -6,14 +6,15 @@ source('~/GitHub/Projects/SEO/standalone/')
 # rm(list = ls(all.names = TRUE)); try(dev.off(), silent = T)
 
 # Functions ------------------------
-require(Stringendo); require(ReadWriter); require(CodeAndRoll2); require(ggExpress); require(MarkdownReports); require(Seurat.utils)
+require(Stringendo); require(ReadWriter); require(CodeAndRoll2); require(MarkdownHelpers); require(ggExpress); require(MarkdownReports); require(Seurat.utils)
 try(source('~/GitHub/Packages/Rocinante/R/Rocinante.R'),silent= F)
+# source('~/GitHub/Packages/Seurat.pipeline/Load.packages.local.R')
 
 # source('https://raw.githubusercontent.com/vertesy/Seurat.Pipeline/main/elements/Load.packages.local.R')
 # try(source("~/GitHub/Packages/Seurat.multicore/00.Load.Seurat3.Multicore.LOCAL.R"));
 
 # Setup ------------------------
-OutDirOrig = OutDir = "~/Dropbox/Abel.IMBA/AnalysisD/"
+OutDirOrig = OutDir = "~/Dropbox (VBC)/Abel.IMBA/AnalysisD/"
 setup_MarkdownReports(OutDir = OutDir, scriptname = "")
 
 
