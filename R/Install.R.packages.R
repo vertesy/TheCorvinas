0acaac59657d61ad0d34fa529be8131577174cba
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-12-11 08:35:02 +0100

    Update Install.R.packages.R

--- R/Install.R.packages.R
+++ R/Install.R.packages.R
@@ -2,6 +2,57 @@
 # A collection of custom R functions
 ######################################################################
 
+
+
+install.packages("tidyverse")
+install.packages("Seurat")
+install.packages("clipr")
+install.packages("doMC")
+install.packages("tictoc")
+install.packages("pheatmap")
+install.packages("biomaRt")
+# If you don't have it
+install.packages("openxlsx")
+install.packages("plotrix")
+install.packages("princurve")
+install.packages("HGNChelper")
+install.packages("R.utils")
+# install.packages("limma")
+install.packages("SoupX")
+
+# BioConductor---------------------------------------------------------------------------------
+install.packages("BiocManager")
+BiocManager::install("schex")
+BiocManager::install("biomaRt")
+BiocManager::install("STRINGdb")
+BiocManager::install("sparseMatrixStats")
+BiocManager::install("limma")
+
+update.packages(ask = F)
+
+# Github---------------------------------------------------------------------------------
+install.packages("devtools")
+require("devtools")
+
+devtools::install_github(repo = "jalvesaq/colorout", upgrade = F)
+"maybe requires X11"
+
+
+devtools::install_github(repo = "vertesy/Stringendo", upgrade = F)
+devtools::install_github(repo = "vertesy/CodeAndRoll2", upgrade = F)
+devtools::install_github(repo = "vertesy/ReadWriter", upgrade = F)
+devtools::install_github(repo = "vertesy/MarkdownHelpers", upgrade = F)
+devtools::install_github(repo = "vertesy/ggExpress", upgrade = F)
+devtools::install_github(repo = "vertesy/Markdownreports", upgrade = F)
+devtools::install_github(repo = "vertesy/Seurat.utils", upgrade = F)
+
+# Less important ones
+devtools::install_github(repo = "vertesy/DataInCode", upgrade = F)
+devtools::install_github(repo = "vertesy/DatabaseLinke.R", upgrade = F)
+
+
+
+
 ## Setup   -------------------------------------------------------------------------------------------------
 install.packages("BiocManager")
 install.packages("tidyverse")
@@ -30,5 +81,13 @@ install.packages("Seurat")
 
 
 require("devtools")
-devtools::install_github(repo = "vertesy/MarkdownReportsDev")
+devtools::install_github(repo = "vertesy/Stringendo", upgrade = F)
+devtools::install_github(repo = "vertesy/CodeAndRoll2", upgrade = F)
+devtools::install_github(repo = "vertesy/ReadWriter", upgrade = F)
+devtools::install_github(repo = "vertesy/MarkdownHelpers", upgrade = F)
+devtools::install_github(repo = "vertesy/ggExpress", upgrade = F)
+devtools::install_github(repo = "vertesy/Markdownreports", upgrade = F)
+devtools::install_github(repo = "vertesy/Seurat.utils", upgrade = F)
+devtools::install_github(repo = "vertesy/DataInCode", upgrade = F)
+devtools::install_github(repo = "vertesy/DatabaseLinke.R", upgrade = F)
 
