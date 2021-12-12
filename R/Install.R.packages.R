dc8af0977120431952bb61fec6ead0d8d4021077
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-12-12 17:43:03 +0100

    Update Install.R.packages.R

--- R/Install.R.packages.R
+++ R/Install.R.packages.R
@@ -1,12 +1,12 @@
 ######################################################################
-# A collection of custom R functions
+# Install R packages
 ######################################################################
-
-
 
 install.packages("tidyverse")
 install.packages("Seurat")
+
 install.packages("clipr")
+install.packages("tictoc")
 install.packages("doMC")
 install.packages("tictoc")
 install.packages("pheatmap")
@@ -19,6 +19,17 @@ install.packages("HGNChelper")
 install.packages("R.utils")
 # install.packages("limma")
 install.packages("SoupX")
+
+install.packages("colorRamps")
+install.packages("data.table")
+install.packages("readr")
+install.packages("sm")
+install.packages("gplots")
+install.packages("gtools")
+install.packages("RColorBrewer")
+install.packages("vioplot")
+install.packages("VennDiagram")
+
 
 # BioConductor---------------------------------------------------------------------------------
 install.packages("BiocManager")
@@ -53,41 +64,22 @@ devtools::install_github(repo = "vertesy/DatabaseLinke.R", upgrade = F)
 
 
 
-## Setup   -------------------------------------------------------------------------------------------------
-install.packages("BiocManager")
-install.packages("tidyverse")
-install.packages("devtools")
-# install.packages("bookdown")
-install.packages("sm")
-install.packages("gplots")
-install.packages("gtools")
+## Else   -------------------------------------------------------------------------------------------------
 
-install.packages("RColorBrewer")
-install.packages("colorRamps")
-install.packages("vioplot")
-install.packages("VennDiagram")
-# install.packages("rmarkdown")
-# install.packages("commonmark")
-install.packages("clipr")
-install.packages("data.table")
-install.packages("pheatmap")
-# install.packages("revealjs")
-install.packages("readr")
-install.packages("Seurat")
-# install.packages("robustbase")
-# devtools::install_github(repo = "vertesy/MarkdownReportsDev")
-# devtools::install_github("csgillespie/roxygen2Comment")
-
-
-
-require("devtools")
-devtools::install_github(repo = "vertesy/Stringendo", upgrade = F)
-devtools::install_github(repo = "vertesy/CodeAndRoll2", upgrade = F)
-devtools::install_github(repo = "vertesy/ReadWriter", upgrade = F)
-devtools::install_github(repo = "vertesy/MarkdownHelpers", upgrade = F)
-devtools::install_github(repo = "vertesy/ggExpress", upgrade = F)
-devtools::install_github(repo = "vertesy/Markdownreports", upgrade = F)
-devtools::install_github(repo = "vertesy/Seurat.utils", upgrade = F)
-devtools::install_github(repo = "vertesy/DataInCode", upgrade = F)
-devtools::install_github(repo = "vertesy/DatabaseLinke.R", upgrade = F)
-
+#
+# # MULTI-seq
+# devtools::install_github('chris-mcginnis-ucsf/MULTI-seq', force = TRUE)
+# require("deMULTIplex") # This is MULTI-seq
+#
+# BiocManager::install("ShortRead")
+#
+# BiocManager::install("DropletUtils")
+#
+#
+#
+#
+# library(devtools)
+# install_github("immunogenomics/harmony")
+# devtools::install_github("immunogenomics/presto")
+#
+# devtools::install_local("/Users/abel.vertesy/Downloads/harmony")
